c34f0533010d6cb26a7e5ab74ec928851027e356
SANKAR RAM <113595362+SANKAR-RAM-0@users.noreply.github.com>
2024-07-01 19:17:48 +0530

    Add files via upload

---
+++ Homepage/Script.js
@@ -0,0 +1,12 @@
+
+function Hidesidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+// function Showsidebar() {
+//     const sidebar = document.querySelector('.sidebar')
+//     sidebar.style.display = 'flex' 
+// }
+
+
